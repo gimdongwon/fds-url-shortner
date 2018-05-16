d59f5e14ac53c4edd791ab96678241edc20d4626
kimdongwon <ehddnjs8989@gmail.com>
2018-05-16 16:14:57 +0900

    link success

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const express = require("express");
 const morgan = require("morgan");
-
+const randomstring = require('randomstring')
 const app = express();
 app.use(morgan("dev"));
 app.use('/static', express.static('public'))
 
 app.get('/',(req,res)=>{
-  res.render('index.ejs');
+  const host = (req.get('host'))
+  res.render('index.ejs', {host, urls});
 })
 
+const urls = [
+  {
+    slug: randomstring.generate(8),
+    longUrl: 'https://www.naver.com'
+  }
+]
+
+app.get('/:slug',(req,res)=>{
+  const urlItem = urls.find(item=> item.slug===req.params.slug)
+  if(urlItem){
+    res.redirect(301, urlItem.longUrl)
+  }else{
+    res.status(404)
+    res.send('404 Not Found')
+  }
+})
 
 app.listen(3000, () => {
   console.log("listening...");
